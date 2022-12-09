f90e27aaae35a66d570391d5bae79fb1d4f8428f
Rocco Lucciola <47836741+mrlucciola@users.noreply.github.com>
2022-12-09 15:24:10 -0500

    feat: #5 add react router (#11)
    add <Nav />, <Body /> and <NotFound /> components
    add react router boilerplate
    add routes to `Body` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+// components
+import Nav from "./components/Nav";
+import Body from "./components/Body";
+
+/** Top level component
+ */
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Body />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Body.tsx
@@ -0,0 +1,47 @@
+// react
+import { Outlet, Route, Routes } from "react-router-dom";
+// components
+import NotFound from "./NotFound";
+
+/** The default layout for the page. To be applied to all routes.
+ */
+const PageLayout: React.FC = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexFlow: "column",
+        flex: 1,
+        overflow: "scroll",
+        alignItems: "center",
+        height: "100%",
+        minHeight: "100%",
+        width: "100%",
+        maxWidth: "100%",
+        background: "grey",
+      }}
+    >
+      <Outlet />
+    </div>
+  );
+};
+
+const PlayerCards = () => <div>Player Cards</div>;
+
+/** Body: Display Component with React Router
+ *
+ * Displays the application's content and sits immediately below the navbar.\
+ * Scrolling occurs internally. Set in PageLayout component
+ */
+const Body: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<PageLayout />}>
+        {/* <Route index element={<SampleIndexComponent />} /> */}
+        <Route path="cards/:playerSlugs" element={<PlayerCards />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+export default Body;
